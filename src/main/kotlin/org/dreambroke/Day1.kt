ba191241866235a15eb7e51dcac65626543834ff
DreamCreate <791231666@qq.com>
2023-12-10 03:02:22 +0800

    refactor Day1 code

--- src/main/kotlin/org/dreambroke/Day1.kt
+++ src/main/kotlin/org/dreambroke/Day1.kt
@@ -11,103 +11,59 @@ object Day1 : BaseProblem() {
         val lines = Utils.splitStringByLine(input)
         var sum = 0
         for (line in lines) {
-            var i = 0
-            var j = line.length - 1
-            var isFirstFound = false
-            var isLastFound = false
-            while (i <= j) {
-                val ci = line[i]
-                val cj = line[j]
-                if (ci.isDigit()) {
-                    isFirstFound = true
-                }
-                if (cj.isDigit()) {
-                    isLastFound = true
-                }
-                if (isFirstFound && isLastFound) {
-                    sum += Integer.parseInt(ci + "" + cj)
-                    break
-                }
-                if (!isFirstFound) {
-                    i++
-                }
-                if (!isLastFound) {
-                    j--
-                }
+            val (first, last) = findFirstAndLastDigits(line, null)
+            if (first != null && last != null) {
+                sum += (first + last).toInt()
             }
         }
+
         return sum.toString()
     }
 
     override fun partB(input: String): String {
-        val digitsInWords = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
         val lines = Utils.splitStringByLine(input)
+        val digitsInWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
         var sum = 0
         for (line in lines) {
-            var i = 0
-            var j = line.length - 1
-            var first: String? = null
-            var last: String? = null
-            while (i <= j) {
-                if (first == null) {
-                    for (ie in 1 .. 5) {
-                        val str = line.substring(i, i + ie)
-                        if (str.isInt()) {
-                            first = str
-                            break
-                        }
-                        if (digitsInWords.keys.any { it.startsWith(str) }) {
-                            val firstFind = digitsInWords.keys.find { it == str }
-                            if (firstFind != null) {
-                                first = digitsInWords[firstFind].toString()
-                            } else {
-                                continue
-                            }
-                        } else {
-                            break
-                        }
-                    }
-                }
-                if (last == null) {
-                    for (js in 1 .. 5) {
-                        val str = line.substring(j - js + 1, j + 1)
-                        if (str.isInt()) {
-                            last = str
-                            break
-                        }
-                        if (digitsInWords.keys.any { it.endsWith(str) }) {
-                            val lastFind = digitsInWords.keys.find { it == str }
-                            if (lastFind != null) {
-                                last = digitsInWords[lastFind].toString()
-                            } else {
-                                continue
-                            }
-                        } else {
-                            break
-                        }
-                    }
-                }
-                if (first != null && last != null) {
-                    sum += Integer.parseInt(first + "" + last)
-                    break
-                }
-                if (first == null) {
-                    i++
-                }
-                if (last == null) {
-                    j--
-                }
+            val (first, last) = findFirstAndLastDigits(line, digitsInWords)
+            if (first != null && last != null) {
+                sum += (first + last).toInt()
             }
         }
+
         return sum.toString()
     }
 
-    private fun String.isInt(): Boolean {
-        return try {
-            this.toInt()
-            true
-        } catch (e: NumberFormatException) {
-            false
+    private fun findFirstAndLastDigits(line: String, digitsInWords: List<String>?): Pair<String?, String?> {
+        var i = 0
+        var j = line.length - 1
+        var first: String? = null
+        var last: String? = null
+
+        while (i <= j) {
+            if (first == null) {
+                first = if (line[i].isDigit()) line[i].toString()
+                else if (digitsInWords != null && line.substring(i).startsWithAny(digitsInWords))
+                    digitsInWords.find { line.substring(i).startsWith(it) }?.let { (digitsInWords.indexOf(it) + 1).toString() }
+                else null
+            }
+
+            if (last == null) {
+                last = if (line[j].isDigit()) line[j].toString()
+                else if (digitsInWords != null && line.substring(0, j + 1).endsWithAny(digitsInWords))
+                    digitsInWords.find { line.substring(0, j + 1).endsWith(it) }?.let { (digitsInWords.indexOf(it) + 1).toString() }
+                else null
+            }
+
+            if (first != null && last != null) return Pair(first, last)
+            if (first == null) i++
+            if (last == null) j--
         }
+
+        return Pair(null, null)
     }
+
+    private fun String.startsWithAny(list: List<String>) = list.any { this.startsWith(it) }
+
+    private fun String.endsWithAny(list: List<String>) = list.any { this.endsWith(it) }
 }
