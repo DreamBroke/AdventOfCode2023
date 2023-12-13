5269cf089d4d367e69cf240c0acda568db6e4b22
DreamCreate <791231666@qq.com>
2023-12-14 01:50:05 +0800

    submit day2 part-B code and refactor Day2.kt

--- src/main/kotlin/org/dreambroke/Day2.kt
+++ src/main/kotlin/org/dreambroke/Day2.kt
@@ -1,6 +1,31 @@
 package org.dreambroke
 
 object Day2 : BaseProblem() {
+
+    data class CubeCounts(val red: Int, val green: Int, val blue: Int)
+
+    private fun handleLine(line: String, action: (CubeCounts) -> Unit) {
+        val cubesGroupStr = line.substringAfter(":").trim()
+        val cubesGroup = cubesGroupStr.split(";")
+        var maxRedCubesCount = 0
+        var maxGreenCubesCount = 0
+        var maxBlueCubesCount = 0
+        for (cubes in cubesGroup) {
+            val singleCubeList = cubes.split(",")
+            for (cube in singleCubeList) {
+                val singleCube = cube.trim().split(" ")
+                val count = singleCube[0].toInt()
+                val color = singleCube[1]
+                when (color) {
+                    "red" -> if (count > maxRedCubesCount) maxRedCubesCount = count
+                    "green" -> if (count > maxGreenCubesCount) maxGreenCubesCount = count
+                    "blue" -> if (count > maxBlueCubesCount) maxBlueCubesCount = count
+                }
+            }
+        }
+        action(CubeCounts(maxRedCubesCount, maxGreenCubesCount, maxBlueCubesCount))
+    }
+
     @JvmStatic
     fun main(args: Array<String>) {
         solvePartA()
@@ -8,36 +33,30 @@ object Day2 : BaseProblem() {
     }
 
     override fun partA(input: String): String {
+        val totalBlueCubes = 14
+        val totalGreenCubes = 13
+        val totalRedCubes = 12
         val lines = Utils.splitStringByLine(input)
         var result = 0
         for (line in lines) {
-            val cubesGroupStr = line.substringAfter(":").trim()
-            val cubesGroup = cubesGroupStr.split(";")
-            var maxRedCubesCount = 0
-            var maxGreenCubesCount = 0
-            var maxBlueCubesCount = 0
-            for (cubes in cubesGroup) {
-                val singleCubeList = cubes.split(",")
-                for (cube in singleCubeList) {
-                    val singleCube = cube.trim().split(" ")
-                    val count = singleCube[0].toInt()
-                    val color = singleCube[1]
-                    when (color) {
-                        "red" -> if (count > maxRedCubesCount) maxRedCubesCount = count
-                        "green" -> if (count > maxGreenCubesCount) maxGreenCubesCount = count
-                        "blue" -> if (count > maxBlueCubesCount) maxBlueCubesCount = count
-                    }
+            handleLine(line) { cubeCounts ->
+                if (cubeCounts.blue <= totalBlueCubes && cubeCounts.green <= totalGreenCubes && cubeCounts.red <= totalRedCubes) {
+                    val gameId = line.substringBefore(":").trim().split(" ")[1].toInt()
+                    result += gameId
                 }
-            }
-            if (maxBlueCubesCount <= 14 && maxGreenCubesCount <= 13 && maxRedCubesCount <= 12) {
-                val gameId = line.substringBefore(":").trim().split(" ")[1].toInt()
-                result += gameId
             }
         }
         return result.toString()
     }
 
     override fun partB(input: String): String {
-        return ""
+        val lines = Utils.splitStringByLine(input)
+        var result = 0
+        for (line in lines) {
+            handleLine(line) { cubeCounts ->
+                result += cubeCounts.blue * cubeCounts.green * cubeCounts.red
+            }
+        }
+        return result.toString()
     }
 }
